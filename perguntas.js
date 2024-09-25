0393c9f41db1ef021bc355f9194e8d68cd1a60dc
BKANITZ <161774270+BKANITZ@users.noreply.github.com>
2024-09-25 19:28:15 -0300

    Update perguntas.js

--- perguntas.js
+++ perguntas.js
@@ -1,29 +1,49 @@
 criaCartao(
-    'Programação',
-    'O que é Python?',
-    'O Python é uma linguagem de programação'
+    'Quem foi a primeira pessoa a viajar no Espaço?',
+    'Yuri Gagarin'
 )
 
 criaCartao(
-    'Geografia',
-    'Qual a capital da França?',
-    'A capital da França é Paris'
+    'Qual a montanha mais alta do mundo?',
+    'Monte Everest'
 )
 
 criaCartao(
-    'Programação',
-    'O que é uma função?',
-    'Uma função é um bloco de código que executa alguma tarefa'
+    'Que país tem o formato de uma bota?',
+    'Itália'
 )
 
 criaCartao(
-    'Lingua inglesa',
-    'Como se diz oi em Inglês?',
-    'Oi em ingles é HI (RAI)'
+    ' O que é mais pesado: 1 quilo de algodão ou 1 quilo de ferro?',
+    'Ambos têm o mesmo peso: 1 quilo. Mas, como precisamos de uma porção de algodão maior para compor 1 quilo, temos a sensação de que o mesmo peso de algodão é mais leve do que o chumbo'
 )
 
 criaCartao(
-    'Nova Pergunta',
-    'Isso é um teste?',
-    'SIM'
+    'Quem inventou a lâmpada?',
+    'Thomas Edison'
+)
+
+criaCartao(
+    ' Quanto tempo a Terra demora para dar uma volta completa em torno dela mesma?',
+    'Aproximadamente 24 horas'
+)
+
+criaCartao(
+    'Quais são as fases da Lua?',
+    'Nova, crescente, cheia e minguante'
+)
+
+criaCartao(
+    'A que temperatura a água ferve??',
+    '100 ºC, é conhecido como o ponto de ebulição da água'
+)
+
+criaCartao(
+    'Quantos ossos temos no nosso corpo?',
+    'O corpo humano tem 206 ossos: 22 na cabeça, 1 no pescoço, 6 no ouvido, 44 no tórax, 7 no abdômen, 62 nos membros inferiores e 64 nos membros inferiores.'
+)
+
+criaCartao(
+    'Qual o maior planeta do sistema solar?',
+    'JÚPITER é 1300 vezes maior que a Terra, sendo o maior planeta do sistema solar'
 )
