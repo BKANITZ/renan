927275ca917cee9f65f32e4db5e6d16510beb3ca
BKANITZ <161774270+BKANITZ@users.noreply.github.com>
2024-10-02 19:23:39 -0300

    Update perguntas.js

--- perguntas.js
+++ perguntas.js
@@ -1,49 +1,59 @@
 criaCartao(
+    'CIENCIAS',
     'Quem foi a primeira pessoa a viajar no Espaço?',
     'Yuri Gagarin'
 )
 
 criaCartao(
+    'GEOGRAFIA',
     'Qual a montanha mais alta do mundo?',
     'Monte Everest'
 )
 
 criaCartao(
-    'Que país tem o formato de uma bota?',
-    'Itália'
+    'GEOGRAFIA',
+    'Quantos países existem?',
+    '195 países'
 )
 
 criaCartao(
-    ' O que é mais pesado: 1 quilo de algodão ou 1 quilo de ferro?',
-    'Ambos têm o mesmo peso: 1 quilo. Mas, como precisamos de uma porção de algodão maior para compor 1 quilo, temos a sensação de que o mesmo peso de algodão é mais leve do que o chumbo'
+    'GEOGRAIA',
+    'O país mais pobre do mundo?',
+    'Burundi, África'
 )
 
 criaCartao(
+    'CIENCIAS',
     'Quem inventou a lâmpada?',
     'Thomas Edison'
 )
 
 criaCartao(
-    ' Quanto tempo a Terra demora para dar uma volta completa em torno dela mesma?',
+    'GEOGRAFIA',
+    'Quanto tempo a Terra demora para dar uma volta completa em torno dela mesma?',
     'Aproximadamente 24 horas'
 )
 
 criaCartao(
+    'CIENCIAS',
     'Quais são as fases da Lua?',
     'Nova, crescente, cheia e minguante'
 )
 
 criaCartao(
+    'QUÍMICA',
     'A que temperatura a água ferve??',
     '100 ºC, é conhecido como o ponto de ebulição da água'
 )
 
 criaCartao(
+    'CIENCIAS',
     'Quantos ossos temos no nosso corpo?',
     'O corpo humano tem 206 ossos: 22 na cabeça, 1 no pescoço, 6 no ouvido, 44 no tórax, 7 no abdômen, 62 nos membros inferiores e 64 nos membros inferiores.'
 )
 
 criaCartao(
+    'GEOGRAFIA',
     'Qual o maior planeta do sistema solar?',
     'JÚPITER é 1300 vezes maior que a Terra, sendo o maior planeta do sistema solar'
 )
